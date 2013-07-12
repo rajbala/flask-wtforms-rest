cbaaa66d5a15a30570bc12af8a1e6484bf4ec32f
Raj Bala <raj@rajbala.com>
2013-07-12 12:53:32 -0700

    Forgot to add the wrapper itself.

---
+++ RestModule/RestWrapper.py
@@ -0,0 +1,46 @@
+import urllib2, urllib
+import re, urlparse, json
+
+base_url = "HTTP LOCATION OF YOUR WEBSERVICE API"
+
+class BetaUser(object):
+    
+    def __init__(self):
+        """ Constructor """
+        
+        #Do setup here if needed
+
+    def add_to_beta(self, email_address):
+        beta_user = {}
+        beta_user['email_address'] = email_address;        
+        json_data = json.dumps(beta_user)
+        
+        request = RequestWithMethod("POST", "%s" % (base_url + "/beta/"))
+        request.add_header('content-type', 'application/json')
+        request.add_data(json_data)
+        error = None
+        response = None
+        try:
+            response = self.__send_request(request)
+            
+        except urllib2.HTTPError, err:
+            pass
+        else:
+            print "No error received, do something here if needed"
+                
+        return (response, error)
+        
+    def __send_request(self, request):
+
+        response = urllib2.urlopen(request)
+        return response
+
+# Subclass the urllib2.Request object and then override the HTTP method
+class RequestWithMethod(urllib2.Request):
+
+    def __init__(self, method, *args, **kwargs):
+        self._method = method
+        urllib2.Request.__init__(self, *args, **kwargs)
+   
+    def get_method(self):
+        return self._method
